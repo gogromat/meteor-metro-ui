066765a10fec4ec248f2781e851aa9836d580779
gogromat <gogromat@gmail.com>
2013-02-16 00:06:28 -0500

    implementing font-face solution

--- package.js
+++ package.js
@@ -5,29 +5,34 @@ Package.describe({
 Package.on_use(function (api) {
 	// CSS FILES
 	//'metrouicss/css/theme-dark.css',
-	api.add_files('css/modern.css', 'client');
-	api.add_files('css/modern-responsive.css', 'client');
+	api.add_files('metrouicss/css/modern.css', 			 'client');
+	api.add_files('metrouicss/css/modern-responsive.css','client');
 
 	// JAVASCRIPT
 	//'metrouicss/public/js/assets/jquery.mousewheel.min.js',
 	//'metrouicss/javascript/start-menu.js',], 'client');
 		//'metrouicss/public/css/prettify.css',
-	api.add_files('metrouicss2/public/js/assets/jquery-1.9.0.min.js', 	'client');
-	api.add_files('metrouicss2/javascript/dropdown.js', 			  		'client');
-	api.add_files('metrouicss2/javascript/accordion.js', 				'client');
-	api.add_files('metrouicss2/javascript/buttonset.js', 				'client');
+	api.add_files('metrouicss/public/js/assets/jquery-1.9.0.min.js','client');
+	api.add_files('metrouicss/javascript/dropdown.js', 'client');
+	api.add_files('metrouicss/javascript/accordion.js','client');
+	api.add_files('metrouicss/javascript/buttonset.js','client');
 		//'metrouicss/javascript/carousel.js', 
-	api.add_files('metrouicss2/javascript/input-control.js', 			'client');
-	api.add_files('metrouicss2/javascript/pagecontrol.js', 				'client');
+	api.add_files('metrouicss/javascript/input-control.js','client');
+	api.add_files('metrouicss/javascript/pagecontrol.js','client');
 		//'metrouicss/javascript/rating.js', 
 		//'metrouicss/javascript/slider.js', 
-	api.add_files('metrouicss2/javascript/dialog.js', 					'client');
-	api.add_files('metrouicss2/javascript/tile-slider.js', 				'client');
-	api.add_files('metrouicss2/javascript/tile-drag.js', 				'client');
+	api.add_files('metrouicss/javascript/dialog.js', 	 'client');
+	api.add_files('metrouicss/javascript/tile-slider.js','client');
+	api.add_files('metrouicss/javascript/tile-drag.js',  'client');
 
 	// FONTS
-	api.add_files('fonts/iconFont.woff', 'client');
-	api.add_files('fonts/iconFont.eot', 'client');
-	api.add_files('fonts/iconFont.ttf', 'client');
-	api.add_files('fonts/iconFont.svg', 'client');
+	api.add_files('metrouicss/fonts/iconFont.woff', 'client');
+	api.add_files('metrouicss/fonts/iconFont.eot',  'client');
+	api.add_files('metrouicss/fonts/iconFont.ttf',  'client');
+	api.add_files('metrouicss/fonts/iconFont.svg',  'client');
+
+
+	// XXX Similar to bootstrap-jasny, because of absolute path
+	// we need to rewrite font-face rules to point to /packages/metrouicss/...
+	api.add_files('metrouicss-override.css', 'client');
 });
